3a043715ecebdfe68b94d73aa2c1174296085242
Mariano Kaimakamian Carrau <mkc@outlook.com.ar>
2018-09-30 11:07:08 -0300

    Refactorización del mailer para el uso de templates.

--- Helper/MailerHelper.cs
+++ Helper/MailerHelper.cs
@@ -35,7 +35,6 @@ namespace Helper
                     mail.To.Add(new MailAddress(recipient));
                 }
 
-                //Formatted mail body
                 mail.IsBodyHtml = true;
                 mail.Subject = subject;
                 mail.Body = body;
@@ -49,49 +48,65 @@ namespace Helper
             }
 
         public static void SendWelcomeMail(User user, String activationHash)
-        {           
-            Send(
-            "Gracias por registrarte, " + user.Name + "!",
-            "Estás recibiendo este mail porque te has registrado y queremos verificar tu identidad." +
-            Environment.NewLine +
-            "Por favor, accedé a la siguiente url: http://localhost:50551/register_action.aspx?a=" + activationHash,
-            new string[] { user.Mail });
+        {
+            string body = GetTemplate(
+                user.Name,
+                "Estás recibiendo este mail porque te has registrado y queremos verificar tu identidad.",
+                "http://localhost:50551/register_action.aspx?a=" + activationHash
+                );
+
+            Send("Gracias por registrarte, " + user.Name + "!", body, new string[] { user.Mail });
         }
 
         public static void SendResetPassword(User user, String activationHash)
         {
-            string path = ConfigurationManager.AppSettings["MAIL_TEMPLATE_PATH"];
-            string fullPath = HttpContext.Current.Server.MapPath(path);
+            string body = GetTemplate(
+                user.Name,
+                "Estás recibiendo este mail porque nos informaste que te olvidaste tu password.",
+                "http://localhost:50551/recovery_action.aspx?a=" + activationHash
+                );
 
-            StreamReader streamReader = new StreamReader(fullPath);
-            String body = streamReader.ReadToEnd();
-            streamReader.Dispose();
-
-            body = body.Replace("@usuario", user.Name).Replace(
-                "@motivo",
-                "Estás recibiendo este mail porque nos informaste que te olvidaste tu password."
-                ).Replace("@web", "http://localhost:50551/recovery_action.aspx?a=" + activationHash);
-
-            Send("Recupero de password - Ubiquicity", body, new string[] { user.Mail });
+            Send("Recupero de password - Ubiquicity", body, new string[] {user.Mail});
         }
 
         public static void SendResetPasswordByAdmin(User user, String activationHash)
         {
-            Send(
-            "Reseteo de password - Ubiquicity",
-            "Estás recibiendo este mail porque el administrador reseteó tu password y requiere que lo cambies." +
-            Environment.NewLine +
-            "Por favor, accedé a la siguiente url para resetear el password: http://localhost:50551/recovery_action.aspx?a=" + activationHash,
-            new string[] { user.Mail });
+            string body = GetTemplate(
+                user.Name,
+                "Estás recibiendo este mail porque el administrador reseteó tu password y requiere que lo cambies.",
+                "http://localhost:50551/recovery_action.aspx?a=" + activationHash
+                );
+
+            Send("Reseteo de password - Ubiquicity", body, new string[] {user.Mail});
         }
 
         public static void SendResetPasswordConfirmation(User user)
         {
-            Send(
-            "Password cambiado - Ubiquicity",
-            "Estás recibiendo este mail para recordarte que has cambiado tu password." +
-            Environment.NewLine +  "Esperamos que no te la olvides otra vez ^_~",
-            new string[] { user.Mail });
+            string body = GetTemplate(
+                user.Name,
+                "Estás recibiendo este mail para recordarte que has cambiado tu password." +
+                Environment.NewLine + "Esperamos que no te la olvides otra vez ^_~",
+                "http://localhost:50551/index.aspx"
+                );
+
+            Send("Password cambiado - Ubiquicity", body, new string[] { user.Mail });
+        }
+
+        /// <summary>
+        /// Lee el archivo html y procede al reemplazo de las variables internas con el contenido.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="topic"></param>
+        /// <param name="webUrl"></param>
+        /// <returns></returns>
+        private static string GetTemplate(string user, string topic, string webUrl)
+        {
+            string path = ConfigurationManager.AppSettings["MAIL_TEMPLATE_PATH"];
+            string fullPath = HttpContext.Current.Server.MapPath(path);
+            StreamReader streamReader = new StreamReader(fullPath);
+            String body = streamReader.ReadToEnd();
+            streamReader.Dispose();
+            return body.Replace("@usuario", user).Replace("@motivo",topic).Replace("@web", webUrl);
         }
     }
 }
